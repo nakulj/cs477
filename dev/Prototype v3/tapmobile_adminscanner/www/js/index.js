21338db58a352ae81ee0ab17264ce3db146b36dd
nakulj <nakuljos@usc.edu>
2014-04-21 10:54:51 -0700

    Added validation code

--- dev/Prototype v3/tapmobile_adminscanner/www/js/index.js
+++ dev/Prototype v3/tapmobile_adminscanner/www/js/index.js
@@ -37,10 +37,8 @@ var app = {
         alert("scan");
         cordova.plugins.barcodeScanner.scan(
             function (result) {
-                alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
+                alert(result.txt);
+                process(result.txt);
             }, 
             function (error) {
                 alert("Scanning failed: " + error);
@@ -48,3 +46,39 @@ var app = {
         );
     }
 };
+
+process(str) {
+  words= str.split(" ");
+  uid= words[0];
+  nguests= words[1];
+  time= words[2]
+  hmac= words[3]
+
+  if(!validateTime(time))
+    fail();
+  if(!validateHMAC(uid,nguests,time,hash))
+    fail();
+
+  confirmTAP(uid);
+}
+
+validateTime(time) {
+  var cutoff= 5*60*1000;
+  now= new Date().getTime();
+  diff= now-time;
+  return(diff>cutoff);
+}
+
+validateHash(uid,nguests,time,hash) {
+  toHash= uid+nguests+time;
+  var passphrase= "1337password";
+  return(hash == CryptoJS.HmacSHA1(toHash, passphrase));
+}
+
+fail() {
+  alert("Invalid QR.");
+}
+
+confirmTAP(uid) {
+  alert("Confirm user "+uid);
+}
