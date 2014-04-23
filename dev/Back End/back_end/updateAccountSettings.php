dca86ca89d90ce96d0bdd08202faeb68496a0e41
rishimukhop <Administrator@guest-wireless-upc-nat-206-117-88-083.usc.edu>
2014-04-23 12:04:37 -0700

    Update Account
    Update account

--- dev/Back End/back_end/updateAccountSettings.php
+++ dev/Back End/back_end/updateAccountSettings.php
@@ -10,12 +10,21 @@ $con = mysql_connect("fdb7.biz.nf", "1616563_tap", "tapmobile7")
 $email_changed = trim($_POST["email_changed"]);
 $pass_changed = trim($_POST["email_changed"]);
 $new_email = trim($_POST["email"]);
-$pass = trim($_POST["pass"]);
+$new_pass = trim($_POST["pass"]);
 
-if($email_changed) {
+if($email_changed && !$pass_changed) {
     $query=mysql_query("UPDATE Users SET email='$new_email' WHERE email='$userSession'  ");
+}
 
+if($pass_changed && !$email_changed){
+    $query=mysql_query("UPDATE Users SET password='$new_pass' WHERE email='$userSession'  ");
 
 }
+if($pass_changed && $email_changed) {
+    $query=mysql_query("UPDATE Users SET email='$new_email' WHERE email='$userSession'  ");
+    $query=mysql_query("UPDATE Users SET password='$new_pass' WHERE email='$new_email'  ");
+
+}
+
 
 ?>
