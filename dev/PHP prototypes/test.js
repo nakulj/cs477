dee086983aabf464b620f2d966cc77d02dc4a686
nakulj <nakuljos@usc.edu>
2014-04-29 22:34:07 -0700

    Developing prototype

---
+++ dev/PHP prototypes/test.js
@@ -0,0 +1,21 @@
+function testAndDeduct() {
+    var uid= 9;
+    var nguests=3;
+    var cost= 1.5*(nguests+1);
+    $.ajax({
+        type:'POST',
+        url:'http://tapmobile.co.nf/nack_end/validateDeductBalance',
+        data: {
+            userid: uid,
+            cost: cost
+        },
+        success: function(data) {
+            var validateTAP=$.parseJSON(data);
+            alert('No worries, mate');
+            alert(validateTAP);
+        },
+        error: function(data, textStatus) {
+            alert("EPIC FAIL");
+        }
+    });
+}
